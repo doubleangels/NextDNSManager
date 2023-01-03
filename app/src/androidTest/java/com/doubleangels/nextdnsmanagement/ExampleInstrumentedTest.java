1de61e4a2e4ebb517f0065c20537dcbb655f1a58
mtverlee <matt@doubleangels.com>
2023-01-03 16:34:57 -0700

    Updated unit tests

--- app/src/androidTest/java/com/doubleangels/nextdnsmanagement/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/doubleangels/nextdnsmanagement/ExampleInstrumentedTest.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
 /**
--- app/src/test/java/com/doubleangels/nextdnsmanagement/ExampleUnitTest.java
+++ app/src/test/java/com/doubleangels/nextdnsmanagement/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.doubleangels.nextdnsmanagement;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
