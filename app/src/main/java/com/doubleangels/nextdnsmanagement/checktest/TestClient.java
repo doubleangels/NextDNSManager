fe2d944f9f0d2aa6bcb9f9f880775992123bc21e
mtverlee <matt@doubleangels.com>
2023-06-09 17:42:51 -0600

    Added sentry transaction profiling to getBaseClient()

--- app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
+++ app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
@@ -4,18 +4,26 @@ import android.content.Context;
 
 import com.doubleangels.nextdnsmanagement.R;
 
+import io.sentry.ITransaction;
+import io.sentry.Sentry;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TestClient {
     private static Retrofit retrofit = null;
     public static Retrofit getBaseClient(Context c) {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(c.getString(R.string.test_url))
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        ITransaction TestClient_create_transaction = Sentry.startTransaction("TestClient_getBaseClient()", "TestClient");
+        try {
+            if (retrofit == null) {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(c.getString(R.string.test_url))
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+        } catch (Exception e) {
+            Sentry.captureException(e);
         }
+        TestClient_create_transaction.finish();
         return retrofit;
     }
 }
