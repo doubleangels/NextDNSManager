915a403dca24b8bda768699b6710dd73ca5cd2cd
mtverlee <matt@doubleangels.com>
2023-06-16 01:57:11 -0600

    Removed extra Sentry error handling, already handled in checkInheritedDNS()

--- app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
+++ app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
@@ -4,29 +4,18 @@ import android.content.Context;
 
 import com.doubleangels.nextdnsmanagement.R;
 
-import io.sentry.ITransaction;
-import io.sentry.Sentry;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TestClient {
     private static Retrofit retrofit = null;
     public static Retrofit getBaseClient(Context c) {
-        ITransaction TestClient_create_transaction = Sentry.startTransaction("TestClient_getBaseClient()", "TestClient");
-        try {
-            if (retrofit == null) {
-                retrofit = new Retrofit.Builder()
-                        .baseUrl(c.getString(R.string.test_url))
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-            }
-            TestClient_create_transaction.finish();
-            return retrofit;
-        } catch (Exception e) {
-            Sentry.captureException(e);
-            TestClient_create_transaction.finish();
-            return retrofit;
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(c.getString(R.string.test_url))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
-
+        return retrofit;
     }
 }
