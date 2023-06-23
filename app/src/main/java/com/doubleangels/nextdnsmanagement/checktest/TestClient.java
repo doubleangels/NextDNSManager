ae8ad87a34fa2ccc079c89adc64c75a54d509160
Matt VerLee <matt@doubleangels.com>
2023-06-22 22:51:05 -0600

    Fixed bug with VisualIndicator

--- app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
+++ app/src/main/java/com/doubleangels/nextdnsmanagement/checktest/TestClient.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.doubleangels.nextdnsmanagement.R;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class TestClient {
@@ -14,6 +15,7 @@ public class TestClient {
             retrofit = new Retrofit.Builder()
                     .baseUrl(context.getString(R.string.test_url))
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .build();
         }
         return retrofit;
