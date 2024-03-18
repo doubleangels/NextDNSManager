7831e51418c83edc90cca104ab2bfa97ff5d547a
mtverlee <matt@doubleangels.com>
2024-03-18 16:43:22 -0600

    Final mprovements to dark mode extension

--- app/src/main/assets/darkmode/darkmode.js
+++
@@ -1,7 +0,0 @@
-console.log('Dark mode script loaded.');
-
-addEventListener("DOMContentLoaded", (event) => {
-    console.log('Removing styles.');
-    const stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
-    stylesheets.forEach(sheet => sheet.remove());
-});
